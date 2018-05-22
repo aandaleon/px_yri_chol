0911327dbb546cab41dd3d8770566259d665d9d3
Angela Andaleon <angie.andaleon@gmail.com>
2018-05-22 10:19:41 -0500

    Update and rename makeAnno.R to makeAnno.py

---
+++ 04_GEMMA_GWAS/makeAnno.py
@@ -0,0 +1,11 @@
+import gzip
+dosage_path = "/home/angela/px_his_chol/Imputation/UMich/UMich_dosages/"
+anno_path = "/home/angela/px_his_chol/GEMMA/anno/"
+
+for i in range(1,22):
+    anno = open(anno_path + "anno" + str(i) + ".txt", "w")
+    for line in gzip.open(dosage_path + "chr" + str(i) + ".maf0.01.r20.8.dosage.txt.gz", "rb"):
+        arr = line.strip().split()
+        (chr, rs, bp) = arr[0:3]
+        anno.write(rs + "\t" + bp + "\t" + chr + "\n")
+    anno.close()
